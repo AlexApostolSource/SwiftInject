98f250b219f2e77561fd9e43b57eb3af19acc6dd
AlexApostol <apostol516@gmail.com>
2025-08-17 20:35:45 +0200

    initial Registration

--- SwiftInject/Sources/SwiftInject/SwiftInject.swift
+++ SwiftInject/Sources/SwiftInject/SwiftInject.swift
@@ -8,15 +8,15 @@ public protocol InjectionKey {
 }
 
 /// Provides access to injected dependencies.
-struct InjectedValues: @unchecked Sendable {
+public struct InjectedValues: @unchecked Sendable {
     /// This is only used as an accessor to the computed properties within extensions of `InjectedValues`.
-    nonisolated(unsafe) private static var current = InjectedValues()
+    nonisolated(unsafe)  static var current = InjectedValues()
 
     /// Thread-safe lock for protecting access to dependency values
     private static let lock = NSRecursiveLock()
 
     /// A static subscript for updating the `currentValue` of `InjectionKey` instances.
-    static subscript<K>(key: K.Type) -> K.Value where K : InjectionKey {
+   public static subscript<K>(key: K.Type) -> K.Value where K : InjectionKey {
         get {
             lock.withLock { key.currentValue }
         }
@@ -37,14 +37,54 @@ struct InjectedValues: @unchecked Sendable {
 }
 
 @propertyWrapper
-struct Injected<T>: @unchecked Sendable {
+public struct Injected<T>: @unchecked Sendable {
     private let keyPath: WritableKeyPath<InjectedValues, T>
-    var wrappedValue: T {
+    public var wrappedValue: T {
         get { InjectedValues[keyPath] }
         set { InjectedValues[keyPath] = newValue }
     }
 
-    init(_ keyPath: WritableKeyPath<InjectedValues, T>) {
+    public init(_ keyPath: WritableKeyPath<InjectedValues, T>) {
         self.keyPath = keyPath
+    }
+}
+
+/// Centralized dependency registration container
+public struct DependencyContainer {
+    private static let lock = NSRecursiveLock()
+
+    /// Register a dependency using a key path
+    public static func register<T>(_ keyPath: WritableKeyPath<InjectedValues, T>, _ instance: T) {
+        lock.withLock {
+            InjectedValues[keyPath] = instance
+        }
+    }
+
+    /// Register a dependency using an injection key
+    public static func register<K: InjectionKey>(_ keyType: K.Type, _ instance: K.Value) {
+        lock.withLock {
+            InjectedValues[keyType] = instance
+        }
+    }
+
+    /// Reset all dependencies to their default values (useful for testing)
+    public static func reset() {
+        lock.withLock {
+            InjectedValues.current = InjectedValues()
+        }
+    }
+
+    /// Get the current value of a dependency
+    public static func resolve<T>(_ keyPath: WritableKeyPath<InjectedValues, T>) -> T {
+        lock.withLock {
+            InjectedValues[keyPath]
+        }
+    }
+
+    /// Get the current value using an injection key
+    public static func resolve<K: InjectionKey>(_ keyType: K.Type) -> K.Value {
+        lock.withLock {
+            InjectedValues[keyType]
+        }
     }
 }
