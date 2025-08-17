bccb9387ea769c06fec2837f0c0e4666c1f00382
AlexApostol <apostol516@gmail.com>
2025-08-17 09:57:06 +0200

    initial commit

---
+++ SwiftInject/Package.swift
@@ -0,0 +1,25 @@
+// swift-tools-version: 6.1
+// The swift-tools-version declares the minimum version of Swift required to build this package.
+
+import PackageDescription
+
+let package = Package(
+    name: "SwiftInject",
+    platforms: [.macOS(.v11), .iOS(.v15), .tvOS(.v15), .watchOS(.v9)],
+    products: [
+        // Products define the executables and libraries a package produces, making them visible to other packages.
+        .library(
+            name: "SwiftInject",
+            targets: ["SwiftInject"]),
+    ],
+    targets: [
+        // Targets are the basic building blocks of a package, defining a module or a test suite.
+        // Targets can depend on other targets in this package and products from dependencies.
+        .target(
+            name: "SwiftInject"),
+        .testTarget(
+            name: "SwiftInjectTests",
+            dependencies: ["SwiftInject"]
+        ),
+    ]
+)
